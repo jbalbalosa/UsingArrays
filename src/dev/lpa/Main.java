38ec62c80dfb56042e4836765d373b012c560014
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-30 15:46:37 +0900

    Update Main.java

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,5 +1,64 @@
+package dev.lpa;
+
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] firstArray = getRandomArray(10);
+        System.out.println(Arrays.toString(firstArray));
+        Arrays.sort(firstArray);
+        System.out.println(Arrays.toString(firstArray));
+
+        int[] secondArray = new int[10];
+        System.out.println(Arrays.toString(secondArray));
+        Arrays.fill(secondArray,5);
+        System.out.println(Arrays.toString(secondArray));
+
+        System.out.println("3rd and 4th Arrays");
+        int[] thirdArray = getRandomArray(10);
+        System.out.println(Arrays.toString(thirdArray));
+
+        int[] fourthArray = Arrays.copyOf(thirdArray,thirdArray.length);
+        System.out.println(Arrays.toString(fourthArray));
+        System.out.println("4th Arrays sorted");
+        Arrays.sort(fourthArray);
+        System.out.println(Arrays.toString(fourthArray));
+
+        System.out.println("copy of 3rd Array");
+        int[] smallerArray = Arrays.copyOf(thirdArray, 5);
+        System.out.println(Arrays.toString(smallerArray));
+
+        int[] largerArray = Arrays.copyOf(thirdArray,15);
+        System.out.println(Arrays.toString(largerArray));
+
+        System.out.println("String Array");
+        String[] sArray = {"Able", "Jane","Mark","Ralph", "David"};
+        Arrays.sort(sArray);
+        System.out.println(Arrays.toString(sArray));
+        if (Arrays.binarySearch(sArray, "Mark") >= 0){
+            System.out.println("Found mark in the list");
+        }
+
+        int[] s1 = {1,2,3,4,5};
+        int[] s2 = {5,2,3,4,1,0};
+        if (Arrays.equals(s1,s2)){
+            System.out.println("Arrays are equal");
+        } else{
+            System.out.println("Arrays are not equal ");
+        }
+    }
+
+    private static int[] getRandomArray(int len){
+
+        Random random = new Random();
+        int[] newInt = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            newInt[i] = random.nextInt(100);
+        }
+        return newInt;
     }
 }
